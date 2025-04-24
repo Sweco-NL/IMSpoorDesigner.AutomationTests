f71a1f54ed86547e325c6a94c0d278e6c643623d
Arash Khabbazha <arash.khabbazha@sweco.nl>
2025-04-24 14:48:19 +0200

    very basic test to open a folder and toggle SSP

--- GenericTests.cs
+++ GenericTests.cs
@@ -34,19 +34,16 @@ namespace IMSpoorDesigner.AutomationTests
             var menu = mainWindow.FindFirstDescendant(cf => cf.ByClassName("RibbonApplicationMenu")).AsMenu();
             var menuBtn = menu.FindFirstDescendant(cf => cf.ByClassName("RibbonToggleButton"));
             menuBtn?.Click();
-            var openMenuItem = menu.FindFirstDescendant(cf => cf.ByName("Open ...")).AsMenuItem();
+            var openMenuItem = menu.FindFirstDescendant(cf => cf.ByName("Open Folder")).AsMenuItem();
             openMenuItem?.Click();
 
             Thread.Sleep(2000);
 
             var dialogWindow = mainWindow.ModalWindows[0]; // Get the first modal dialog (File Open dialog)
 
-            var filePathTextBox = dialogWindow.FindFirstDescendant(cf => cf.ByAutomationId("1148")).AsComboBox(); // "1148" is a common AutomationId for file dialogs
+            var filePathTextBox = dialogWindow.FindFirstDescendant(cf => cf.ByAutomationId("1152")).AsComboBox(); // "1148" is a common AutomationId for file dialogs
             var txtInput = filePathTextBox.FindFirstChild().As<TextBox>();
-            txtInput.Focus();
-            txtInput.Text = fileName;
-            txtInput.Patterns.Value.Pattern.SetValue(fileName);
-            var openBtn = dialogWindow.FindFirstChild(cf => cf.ByName("Open")).AsButton();
+            var openBtn = dialogWindow.FindFirstChild(cf => cf.ByName("Select Folder")).AsButton();
             openBtn.Click();
 
 
@@ -59,21 +56,61 @@ namespace IMSpoorDesigner.AutomationTests
             using var app = Application.Launch(AppPath);
             using var automation = new UIA3Automation();
             var mainWindow = app.GetMainWindow(automation);
-            OpenImxFile(app, mainWindow, "2021-11-17_1243_Halfweg.xml");
+            OpenImxFile(app, mainWindow, "ModelStation_TC9l");
           
             //assert//
             var treeView = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("ImxTreeview"));
-            Thread.Sleep(2000);
+            Thread.Sleep(12000);
             var treeItem = treeView.FindFirstChild(cf => cf.ByName("IMSpoor.TreeviewManager.TreeItemViewModels.SituationTreeItemViewModel"));
             
             Assert.IsNotNull(treeItem,"There is no treeviewCreated, IMX not loaded!");
-            
 
+            TestRenderSSPOnMap(app, mainWindow);
+
+            Thread.Sleep(2000);
+            app.Close();
             //app.Close();
 
             // Optionally verify some UI change after clicking the button.
             //var label = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("YourLabelId")).AsLabel();
             // Assert.AreEqual("Expected Text", label.Text, "Label text did not update as expected!");
+        }
+
+        [SupportedOSPlatform("windows")]
+        public void TestRenderSSPOnMap(Application app, Window mainWindow)
+        {
+            var menu = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("mainRibbon")).AsMenu();
+            var analysis = menu.FindFirstDescendant(cf => cf.ByAutomationId("AnalysisRibbonTab")).AsTabItem();
+            var analysisButton = analysis.FindFirstDescendant(cf => cf.ByClassName("RibbonTabHeader"));
+            Assert.IsNotNull(analysisButton, "Analysis tab not found!");
+            Thread.Sleep(2000);
+            analysisButton?.Click();
+
+            var sspList = mainWindow.FindFirstDescendant(cf => cf.ByAutomationId("StaticSpeedProfile")).AsListBox();
+            Thread.Sleep(1000);
+            var sspBasic = sspList.FindFirstDescendant(cf => cf.ByName("Show SSP Basic Speed")).AsButton();
+            var sspCat0 = sspList.FindFirstDescendant(cf => cf.ByName(" Show SSP Category 0")).AsButton();
+            var sspCat1 = sspList.FindFirstDescendant(cf => cf.ByName(" Show SSP Category 1")).AsButton();
+            var sspCat2 = sspList.FindFirstDescendant(cf => cf.ByName(" Show SSP Category 2")).AsButton();
+
+            sspBasic.Click();
+            Thread.Sleep(3000);
+            sspBasic.Click();
+
+            Thread.Sleep(1000);
+            sspCat0.Click();
+            Thread.Sleep(1000);
+            sspCat0.Click();
+
+            Thread.Sleep(1000);
+            sspCat1.Click();
+            Thread.Sleep(1000);
+            sspCat1.Click();
+
+            Thread.Sleep(1000);
+            sspCat2.Click();
+            Thread.Sleep(1000);
+            sspCat2.Click();
         }
     }
 }
